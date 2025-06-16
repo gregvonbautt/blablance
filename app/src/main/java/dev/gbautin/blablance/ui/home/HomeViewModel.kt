9c0e187368e37a73d96536e01507f35159167949
Grisha Bautin <greg.bautin@gmail.com>
2025-06-15 21:56:31 -0400

    Draft basic home screen

--- app/src/main/java/dev/gbautin/blablance/ui/home/HomeViewModel.kt
+++ app/src/main/java/dev/gbautin/blablance/ui/home/HomeViewModel.kt
@@ -6,8 +6,16 @@ import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment!"
+    private val _score = MutableLiveData<Int>().apply {
+        value = 0
     }
-    val text: LiveData<String> = _text
+    val score: LiveData<Int> = _score
+
+    fun incrementScore() {
+        _score.value = (_score.value ?: 0) + 1
+    }
+
+    fun decrementScore() {
+        _score.value = (_score.value ?: 0) - 1
+    }
 }
